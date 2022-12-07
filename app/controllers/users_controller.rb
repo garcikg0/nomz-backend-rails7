020154c1a2d1052667c27f5830c05a09ba5cdee6
garcikg0 <kg.gar91@gmail.com>
2022-12-07 16:26:53 -0500

    devise auth in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(
+            first_name: user_params[:first_name],
+            last_name: user_params[:last_name],
+            email: user_params[:email],
+            username: user_params[:username],
+            password: user_params[:password],
+            password_confirmation: user_params[:password_confirmation]
+        )
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    has_secure_password
+    
+    validates :username, presence: :true
+    validates :email, format: { with: /@/, message: "must have an @" }
 end
